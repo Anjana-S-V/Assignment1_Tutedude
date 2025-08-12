7c6bf62efe8d7c4400a1294045fe87c914458df7
Anjana-S-V <anjanasv333@gmail.com>
2025-08-13 01:27:56 +0530

    Added initial files

---
+++ Challenge.py
@@ -0,0 +1,23 @@
+'''  #celcius----->farenheit
+c=input("Celcius:")
+c=float(c)
+f=(c*9/5)+32
+print("Fahrenheit:",f)
+'''
+'''#farenheit---> celcius
+f=input("Fahrenheit:")
+f=float(f)
+c=(f-32)*5/9
+print("Celcius:",c)
+'''
+#Simple interest calculator
+p=input("P: ")
+n=input("N: ")
+r=input("R: ")
+p=float(p)
+n=float(n)
+r=float(r)
+SI=(p*n*r)/100
+print("Simple interest:",SI)
+
+
---
+++ Task2.py
@@ -0,0 +1,5 @@
+#personalized greeting
+a=str(input("Enter your first name :"))
+b=str(input("Enter your last name :"))
+c=a+b
+print("Hello,"+c+"!"+ " welcome to  the python program")
